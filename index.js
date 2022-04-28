9b764d9b42a7c1ca94718495ec4fc6ca56506ba2
Raiyan109 <kraiyan109@gmail.com>
2022-04-29 01:44:07 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send('running assignment 11')
+})
+
+app.listen(port, () => {
+    console.log('listening port', port)
+})
