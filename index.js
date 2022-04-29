a6e5795061a29d2ba156f7632fd24f374b14ec40
Raiyan109 <kraiyan109@gmail.com>
2022-04-29 14:45:52 +0600

    password username added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -9,6 +10,19 @@ const app = express()
 // Middleware
 app.use(cors())
 app.use(express.json())
+
+// get connection string from mongodb database
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vxdca.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('assignment 11 connected')
+    // perform actions on the collection object
+    client.close();
+});
+
 
 
 app.get('/', (req, res) => {
