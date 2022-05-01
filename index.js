fe713ee30708fa4452846be0a1b4d515d97caddb
Raiyan109 <kraiyan109@gmail.com>
2022-05-01 14:51:19 +0600

    dynamic route api launched

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
 const app = express()
 
-
 // Middleware
 app.use(cors())
 app.use(express.json())
 
-// get connection string from mongodb database
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vxdca.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.asf7a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('assignment 11 connected')
-    // perform actions on the collection object
-    client.close();
-});
+async function run() {
+    try {
+        await client.connect()
+        const itemCollection = client.db('inventoryAssignment').collection('item')
+
+        app.get('/item', async (req, res) => {
+            const query = {}
+            const cursor = itemCollection.find(query)
+            const items = await cursor.toArray()
+            res.send(items)
+        })
+
+        app.get('/item/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const item = await itemCollection.findOne(query)
+            res.send(item)
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
 
 
 
 app.get('/', (req, res) => {
-    res.send('running assignment 11')
+    res.send('running assignment server')
 })
 
 app.listen(port, () => {
-    console.log('listening port from ass-11', port)
+    console.log('listening to port', port)
 })
+
